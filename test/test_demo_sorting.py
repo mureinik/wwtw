04acc29d884c32e0bb6771755e6be52d2e8d01a4
Allon Mureinik <mureinik@gmail.com>
2025-04-30 09:55:17 +0300

    Slide 21: Fix (kill) mutant #3

--- test/test_demo_sorting.py
+++ test/test_demo_sorting.py
@@ -3,7 +3,7 @@ import demo_sorting
 
 class TestDemoSorting(unittest.TestCase):
     def test_qdsort(self):
-        data = [[1, 2, 3], [1, 3, 2]]
+        data = [[1, 2, 3], [1, 3, 2], [2, 1]]
         for datum in data:
             with self.subTest(datum = datum):
                 self.assertEqual(
