68c59eec79f8410d26d6c64266798226441546c2
Allon Mureinik <mureinik@gmail.com>
2025-04-30 16:04:01 +0300

    Slide 11: Replace Comedian's implementation with Nite Owl's

--- demo_sorting/__init__.py
+++ demo_sorting/__init__.py
@@ -3,6 +3,19 @@ from typing import List
 def qdsort(lst: List[int]):
     """
     A Quick and Dirty Sort algorithm for a list of integers in ascending order
-    Comedian's implementation: Just trust me
+    Nite Owl's implementation: Quicksort
     """
-    return lst
+    n = len(lst)
+    if n <= 1:
+        return lst
+
+    # Find the pivot element
+    pivot = lst[n // 2]
+
+    # Partition the list into three parts
+    left = [x for x in lst if x < pivot]
+    middle = [x for x in lst if x == pivot]
+    right = [x for x in lst if x > pivot]
+
+    # Recursively sort the left and right parts
+    return qdsort(left) + middle + qdsort(right)
--- test/test_demo_sorting.py
+++ test/test_demo_sorting.py
@@ -3,5 +3,8 @@ import demo_sorting
 
 class TestDemoSorting(unittest.TestCase):
     def test_qdsort(self):
-        data = [1, 2, 3]
-        self.assertEqual(sorted(data), demo_sorting.qdsort(data))
+        data = [[1, 2, 3], [1, 3, 2]]
+        for datum in data:
+            with self.subTest(datum = datum):
+                self.assertEqual(
+                    sorted(datum), demo_sorting.qdsort(datum))
